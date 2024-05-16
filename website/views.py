702ded394c6d9660c1527f6b22e5304d028f932b
Rahul Kumar <rahul.kumarcs042@gmail.com>
2024-05-17 01:27:17 +0530

    Added Register Page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 def home(request):
     # Check if logging in
@@ -29,4 +29,19 @@ def logout_user(request):
     return redirect("home")
 
 def register_user(request):
-    return render(request, "website/register.html")
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered! Welcome!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "website/register.html", {'form':form})
+
+    return render(request, "website/register.html", {'form':form})
